7930930faa0844f509d33753c09fcd38ed1992a8
AddisBelayhu <addisgithub@gmail.com>
2023-05-19 16:38:24 +0300

    form commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+<body>
+    
+    <h2>Registration Form</h2>
+    <div class="container">
+        <form action="">
+            <div>
+                <label for="">Name</label>
+                <input type="text" name="name" placeholder="Enter your name">
+            </div>
+
+            <div>
+                <label for="">Email</label>
+                <input type="email" name="email" placeholder="Enter your email">
+            </div>
+
+            <div>
+                <label for="">Gender</label>
+                <input type="radio" name="gender">Male
+                <input type="radio" name="gender">Female
+                
+            </div>
+
+            <div>
+                <label for="">Mobile Phone</label>
+                <input type="text" name="mobile" placeholder="Enter your mobile number"/>
+            </div>
+
+            <div>
+                <label for="">Password</label>
+                <input type="password" name="password" placeholder="Enter your password"/>
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
